336f457af0a9e3f1f995fc82613ef94431900782
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-02-24 10:57:17 +0200

    add routing between pages

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,24 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
+
 class SongList extends Component {
     renderSongs() {
-        return this.props.data.songs.map(song => <li key={song.id} className='collection-item'>{song.title}</li>)
+        return this.props.data.songs.map((song) => (
+            <li key={song.id} className='collection-item'>
+                {song.title}
+            </li>
+        ))
     }
 
     render() {
         if (this.props.data.loading) {
             return <div>Loading...</div>
         }
-        return <ul className='collection'>{this.renderSongs()}</ul>
+        return (
+            <div>
+                <ul className='collection'>{this.renderSongs()}</ul>
+                <Link to='/songs/new' className='btn-floating btn-large red right'><i className='material-icons'>add</i></Link>
+            </div>
+        )
     }
 }
 const query = gql`
-    {
-        songs {
-          id
-          title
-        }
-    }  
+  {
+    songs {
+      id
+      title
+    }
+  }
 `
 export default graphql(query)(SongList)
