d5093ab1207060d6b23d120416f24af913ecdcc8
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-02-25 15:02:04 +0200

    add lyric list

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 class LyricList extends Component {
-    render(){
-        return (
-            <ul>Lyric List</ul>
-        )
-    }
+  renderLyrics() {
+    return this.props.lyrics.map(({ id, content }) => (
+      <li className='collection-item' key={id}>
+        {content}
+      </li>
+    ))
+  }
+  render() {
+    return <ul className='collection'>{this.renderLyrics()}</ul>
+  }
 }
 export default LyricList
